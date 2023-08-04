3430c11750860eec8635ab60564b856f513fc8ab
HumbleGh <isaacoseianane@gmail.com>
2023-08-04 19:53:30 +0000

    minor changes

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +9,17 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    Response response =
+        await get('https://jsonplaceholder.typicode.com/todos/1');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
